2082a5cdf86c02beb99414b5984592676435760f
Sebastian Estay <sebastian.estay@icloud.com>
2024-01-19 16:09:03 +0100

    reverse for loop

--- c-scripts/2024-01-03-strstr.c
+++ c-scripts/2024-01-03-strstr.c
@@ -1,7 +1,7 @@
 
 // https://en.cppreference.com/w/c/string/byte/strstr
 // https://en.cppreference.com/w/cpp/string/byte/strcspn
-// sublime test terminus
+// sublime text terminus (alt+1)
 // build and run : ./build.sh && ./main.out
 
 #include <stdio.h>
@@ -20,10 +20,11 @@ int main()
 	while (fgets(buffer, LINE_MAX_LENGTH, ptr) != NULL)
 	{
 		buffer[strcspn(buffer, "\n")] = 0; // remove newline
-		printf("%s : ", buffer);
+		printf("%s : ", buffer); 
+		int left = strlen(buffer);
+		//int right = 0;
 
-		int position = strlen(buffer);
-		int left = 0;
+		int position = 0;
 
 		for (size_t i = 0; i < NUMBER_OF_DIGITS; i++)
 		{
@@ -40,6 +41,29 @@ int main()
 			}
 		}
 		printf("left %d\n", left);
+
+		//int position = 0;
+
+		// for (size_t i = NUMBER_OF_DIGITS; i != 0; --i)
+		// {
+		// 	//printf("%zu : %s\n", i-1, digits[i-1]);
+		// 	//printf("%zu\n", i);
+			
+		// 	if (strstr(buffer, digits[i-1]) != NULL)
+		// 	{
+		// 		//printf("%zu at position %ld : ", i-1, strstr(buffer, digits[i-1]) - buffer);
+		// 		int new_position = strstr(buffer, digits[i-1]) - buffer;
+
+		// 		if (new_position > position)
+		// 		{
+		// 			position = new_position;
+		// 			right = i-1;
+		// 		}
+		// 	}
+		// }
+		// printf("%s : %d%d\n", buffer, left, right); 
+
+		//printf("%s : %d\n", buffer, left);
 	}
 
 	return 0;
